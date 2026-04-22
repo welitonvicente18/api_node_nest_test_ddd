8706fe8b5e4c40595de6dfae175c42d74df21186
Weliton Vicente <welitonvicente6@gmail.com>
2026-04-22 14:20:00 -0300

    Feat: Add getters methods unit test

--- src/users/domain/entities/__tests__/unit/user.entity.spec.ts
+++ src/users/domain/entities/__tests__/unit/user.entity.spec.ts
@@ -10,6 +10,7 @@ describe('UserEntity unit test', () => {
             name: faker.person.fullName(),
             email: faker.internet.email(),
             password: faker.internet.password(),
+            createdAt: faker.date.past(),
         }
 
         sut = new UserEntity(props)
@@ -19,6 +20,29 @@ describe('UserEntity unit test', () => {
         expect(sut.props.name).toBe(props.name)
         expect(sut.props.email).toBe(props.email)
         expect(sut.props.password).toBe(props.password)
+        expect(sut.props.createdAt).toBeInstanceOf(Date)
+    })
+
+    it('Should Gets name defined', () =>{
+        expect(sut.name).toBeDefined()
+        expect(sut.name).toEqual(props.name)
+        expect(typeof sut.props.name).toBe('string')
+    })
+
+    it('Should Gets email defined', () =>{
+        expect(sut.email).toBeDefined()
+        expect(sut.email).toEqual(props.email)
+        expect(typeof sut.props.email).toBe('string')
+    })
+
+    it('Should Gets password defined', () =>{
+        expect(sut.password).toBeDefined()
+        expect(sut.password).toEqual(props.password)
+        expect(typeof sut.props.password).toBe('string')
+    })
+
+    it('Should Gets createdAt defined', () =>{
+        expect(sut.createdAt).toBeDefined()
         expect(sut.props.createdAt).toBeInstanceOf(Date)
     })
 })
